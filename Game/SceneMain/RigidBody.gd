4a81eb53d46c249193034d5b4ffa3fd1f6aa8e2a
hansmariusoveras <hansmariusoveras@gmail.com>
2020-03-20 20:28:27 +0100

    Camera controls

--- Game/SceneMain/RigidBody.gd
+++ Game/SceneMain/RigidBody.gd
@@ -1,8 +1,10 @@
 extends KinematicBody
 
 # Declare member variables here. Examples:
-var gravity = Vector3.DOWN * 12
+var gravity = Vector3.DOWN * 0
 var speed = 4
+var min_speed = 4
+var max_speed = 24
 var jump_speed = 6
 var jump = false
 
@@ -18,6 +20,13 @@ func _physics_process(delta):
 func get_input():
 	velocity.x = 0
 	velocity.z = 0
+	velocity.y = 0
+	
+	if Input.is_key_pressed(KEY_SHIFT):
+		speed = (speed + max_speed*0.01)/(1+0.01)
+	else:
+		speed = min_speed
+	
 	if Input.is_key_pressed(KEY_A):
 		velocity += -transform.basis.x * speed
 	if Input.is_key_pressed(KEY_D):
@@ -26,6 +35,10 @@ func get_input():
 		velocity += -transform.basis.z * speed
 	if Input.is_key_pressed(KEY_S):
 		velocity += transform.basis.z * speed
+	if Input.is_key_pressed(KEY_Q):
+		velocity.y -= speed
+	if Input.is_key_pressed(KEY_E):
+		velocity.y += speed
 	if Input.is_key_pressed(KEY_LEFT):
 		rotate_y(PI/100)
 	if Input.is_key_pressed(KEY_RIGHT):
