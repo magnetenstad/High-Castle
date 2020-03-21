0a91060d89cb21fe17879c9fe3afacf5e64c2354
hansmariusoveras <hansmariusoveras@gmail.com>
2020-03-21 01:05:13 +0100

    bomb pathfinding

--- Game/SceneTurret/Bomb.gd
+++ Game/SceneTurret/Bomb.gd
@@ -1,9 +1,20 @@
 extends KinematicBody
 var velocity = Vector3()
 var gravity = 1
+var target = Vector3()
 
 func _ready():
-	var direction = (Vector3(randf()*2-1, 0, randf()*2-1)).normalized()
+	var enemy_positions = []
+	for enemy in $"/root/Main/World/LevelController/Enemies".get_children():
+		enemy_positions.append(enemy.translation)
+	var minimum_distance = pow(10, 9)
+	var minimum_distance_position = Vector3()
+	for pos in enemy_positions:
+		if (pos.distance_to(translation)) < minimum_distance:
+			minimum_distance = pos.distance_to(translation)
+			minimum_distance_position = pos
+	target = minimum_distance_position
+	var direction = (target - translation).normalized()
 	velocity = 5*direction + 20*Vector3.UP
 
 func _physics_process(delta): 
