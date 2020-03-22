867de4c358cd2000fbb57dab9a62403b673acfac
magnetenstad <tenstad.magne@gmail.com>
2020-03-22 16:57:07 +0100

    Add explosion

--- Game/SceneTurret/Bomb.gd
+++ Game/SceneTurret/Bomb.gd
@@ -1,11 +1,12 @@
 extends KinematicBody
 
+const EXPLOSION = preload("res://SceneTurret/Explosion.tscn")
 
 var velocity = Vector3()
 var gravity = 1
 var target = Vector3()
 var direction = Vector3()
-var is_dying = false
+#var is_dying = false
 
 func _ready():
 	var enemy_positions = []
@@ -27,16 +28,22 @@ func _physics_process(delta):
 	look_at(translation - direction, Vector3.UP)
 	
 	velocity = move_and_slide(velocity, Vector3.UP)
-	if is_on_floor() and not is_dying:
-		$DeathTimer.start()
-		is_dying = true
-
-
+	if is_on_floor():# and not is_dying:
+		#$DeathTimer.start()
+		explode()
+		
 func _on_Area_body_entered(body):
 	if "Enemy" in body.get_name():
 		body.queue_free()
-		queue_free()
+		explode()
 
+func explode():
+	var particles = EXPLOSION.instance()
+	$"/root/Main/World".add_child(particles)
+	particles.transform = transform
+	for child in particles.get_children():
+		child.one_shot = true
+	queue_free()
 
 func _on_DeathTimer_timeout():
 	queue_free()
